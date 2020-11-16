77c5e1fd3364a355f35acfa58d0c425fe184b8bc
Dylan Palmieri <dgpalmieri@gmail.com>
2020-11-15 21:16:25 -0900

    Add dummy build function

---
+++ build.cpp
@@ -0,0 +1,11 @@
+// build.cpp
+// Dylan Palmieri
+// 2020-11-14
+// Dynamic Programming solution for the bridge problem from Homework 2
+
+#include<vector>
+#include "build.hpp"
+
+int build(const int & west, const int & east, const std::vector<std::vector<int>> & bridges){
+    return 0;
+}
---
+++ build.hpp
@@ -0,0 +1,13 @@
+// build.hpp
+// Dylan Palmieri
+// 2020-11-14
+// Header for build.cpp
+
+#ifndef BUILD_HPP
+#define BUILD_HPP
+
+#include<vector>
+
+int build(const int & west, const int & east, const std::vector<std::vector<int>> & bridges);
+
+#endif /* BUILD_HPP */
